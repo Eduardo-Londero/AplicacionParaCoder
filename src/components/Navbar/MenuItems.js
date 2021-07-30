7b8d2c478e65032594ebd7c21f93461ba33ff23a
Eduardo Martin <edulondero2407@gmail.com>
2021-07-30 17:21:29 -0300

    desafio 1 navbar commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url:'#',
+        cNAME: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url:'#',
+        cNAME: 'nav-links'
+    },
+    {   
+        title: 'Products',
+        url:'#',
+        cNAME: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url:'#',
+        cNAME: 'nav-links'
+    },
+    {   title: 'Sign up',
+        url:'#',
+        cNAME: 'nav-links-mobile'      
+    }
+
+]
